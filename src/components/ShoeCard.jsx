47d7f85eff587585c5e228cc87c6b402f188e753
Keith Jasper japitana <keithjapitana1996@gmail.com>
2024-04-24 23:41:50 +0800

    Implementing Hero Section.

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,26 @@
+const ShoeCard = ({ imgURL, bigShoeImg, changeBigShoeImage }) => {
+  const handleClick = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
+  return (
+    <div
+      className={`rounded-xl border-2 ${bigShoeImg === imgURL ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className="flex items-center justify-center rounded-xl bg-card bg-cover bg-center max-sm:p-4 sm:h-40 sm:w-40">
+        {' '}
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ShoeCard;
